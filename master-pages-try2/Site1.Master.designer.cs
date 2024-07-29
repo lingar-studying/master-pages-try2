06ff9e35166ccb586ddb588a5ae2e9c0075f6b0f
lingar <yimprogramming@gmail.com>
2024-07-29 23:27:58 +0300

    Adding the style

--- master-pages-try2/Site1.Master.designer.cs
+++ master-pages-try2/Site1.Master.designer.cs
@@ -40,5 +40,14 @@ namespace master_pages_try2
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
